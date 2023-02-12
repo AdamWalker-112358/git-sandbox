d6bc7f436c5ae12977f272d65854d4f4fb776be2
Adam-Walker <mail.adamwalker@gmail.com>
2023-02-12 18:26:47 +0200

    Revert "Revert "Rest back to birthday commit""
    This reverts commit 555b0cd5694bd754901128918a024e806185c4f9.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import monster from 'monster'
 import witch from 'witch'
+import baby from 'baby'
 import ghost from 'ghost'
-
+import cake from 'cake'
+import sprinkle from 'sprinkle'
+import mouse from 'mouse'
 
 console.log(witch)
 console.log(ghost)
+
+console.log(baby)
+console.log(mouse)
+
+const birthday = cake + sprinkle
